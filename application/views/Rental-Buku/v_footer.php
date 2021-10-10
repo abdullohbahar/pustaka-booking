02241e3a6e48d1fcb5aad07cf631068861ef1565
AbdullohBahar <abdullohbahar@gmail.com>
2021-10-10 20:57:47 +0700

    fixing footer

--- application/views/Rental-Buku/v_footer.php
+++ application/views/Rental-Buku/v_footer.php
@@ -1,7 +1,7 @@
 
     <div class="footer">
       <div class="footer-font">
-        <h4>RentalBuku</h4>
+        <h4><a href="https://www.rentalbuku.net">RentalBuku</a></h4>
       </div>
     </div>
   </body>
